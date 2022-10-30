49c52124d8afd796ae06431627fd91a98b638167
Sebastian Jörz <sjoerz@skyface.de>
2022-10-30 20:01:02 +0100

    Test a test

--- main.cpp
+++ main.cpp
@@ -19,43 +19,6 @@ void setStarttime() { startTime = time(NULL); }
 // elapsedTime in seconds
 int getElapsedTime() { return time(NULL) - startTime; }
 
-// This pipes the output of cmd1 into cmd2.
-void pipe_cmd(char **cmd1, char **cmd2) {
-    int fds[2];  // file descriptors
-    pipe(fds);
-    pid_t pid;
-
-    // child process #1
-    if (fork() == 0) {
-        // Reassign stdin to fds[0] end of pipe.
-        dup2(fds[0], 0);
-
-        // Not going to write in this child process, so we can close this end
-        // of the pipe.
-        close(fds[1]);
-
-        // Execute the second command.
-        execvp(cmd2[0], cmd2);
-        perror("execvp failed");
-
-        // child process #2
-    } else if ((pid = fork()) == 0) {
-        // Reassign stdout to fds[1] end of pipe.
-        dup2(fds[1], 1);
-
-        // Not going to read in this child process, so we can close this end
-        // of the pipe.
-        close(fds[0]);
-
-        // Execute the first command.
-        execvp(cmd1[0], cmd1);
-        perror("execvp failed");
-
-        // parent process
-    } else
-        waitpid(pid, NULL, 0);
-}
-
 char *getPrefixes() {
     char *pwd = getcwd(NULL, 0);
     if (homeDir == NULL) {
